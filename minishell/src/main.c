cac41858e17f0a1e6be84e3bc6e1d9e0f7853406
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-02-18 17:11:00 +0100

    Add token parsing functionality

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 18:00:12 by zelhajou          #+#    #+#             */
+/*   Updated: 2024/02/17 20:15:58 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 int main(void)
 {
     char	*input;
 	t_token	*tokens;
+	//t_ast_node *ast;
 
 	while (1)
     {
         input = readline("minishell$ ");
-		printf("Input: %s\n", input);
         if (!input)
             break;
         if (*input)
@@ -16,7 +28,7 @@ int main(void)
 		// Tokenize input
 		if (!syntax_error_checker(ft_strtrim(input,  " \t\n\v\f\r")))
 		{
-			tokens = tokenize_input(input);
+			tokens = tokenize_input(ft_strtrim(input,  " \t\n\v\f\r"));
 			if (!tokens)
 			{
 				free(input);
@@ -28,9 +40,10 @@ int main(void)
 			free(input);
 			continue;
 		}
+		// Parse input
+		// ast = parse_tokens(tokens);
 		// display the tokens
 		display_tokens(tokens);
-        // Parse input
         // execution of the parsed commands
         free(input);
     }
