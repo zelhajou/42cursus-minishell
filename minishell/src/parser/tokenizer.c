e70db3966b375414acfbb73b28226dda8832aebf
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-02-18 17:10:17 +0100

    Refactor tokenizer.c: Rename Tokenization.c to tokenizer.c and remove unused functions

--- minishell/src/parser/tokenizer.c
+++ minishell/src/parser/tokenizer.c
@@ -1,79 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Tokenization.c                                     :+:      :+:    :+:   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 14:26:48 by zelhajou          #+#    #+#             */
-/*   Updated: 2024/02/15 14:27:22 by zelhajou         ###   ########.fr       */
+/*   Updated: 2024/02/18 17:05:02 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-const char	*get_token_type_name(t_token_type type)
-{
-	const char	*token_type_names[TOKEN_TYPE_COUNT];
-
-	token_type_names[0] = "WORD";
-	token_type_names[1] = "PIPE";
-	token_type_names[2] = "REDIRECT_IN";
-	token_type_names[3] = "REDIRECT_OUT";
-	token_type_names[4] = "REDIRECT_APPEND";
-	token_type_names[5] = "REDIRECT_HEREDOC";
-	token_type_names[6] = "TOKEN_ENV_VAR";
-	token_type_names[7] = "EOF";
-	if (type >= 0 && type < TOKEN_TYPE_COUNT)
-		return (token_type_names[type]);
-	return ("UNKNOWN");
-}
-
-void	display_tokens(t_token *tokens)
-{
-	t_token	*token;
-
-	token = tokens;
-	while (token)
-	{
-		printf("Token: [ %-20s ]\tType: [ %-20s ]\n",
-			token->value, get_token_type_name(token->type));
-		token = token->next;
-	}
-}
-
-t_token	*new_token(t_token_type type, char *value)
-{
-	t_token	*token;
-
-	token = malloc(sizeof(t_token));
-	if (!token)
-		return (NULL);
-	token->type = type;
-	token->value = ft_strdup(value);
-	if (!token->value)
-	{
-		free(token);
-		return (NULL);
-	}
-	token->next = NULL;
-	return (token);
-}
-
-void	add_token_to_list(t_token **tokens, t_token *new_token)
-{
-	t_token	*last;
-
-	if (!*tokens)
-		*tokens = new_token;
-	else
-	{
-		last = *tokens;
-		while (last->next)
-			last = last->next;
-		last->next = new_token;
-	}
-}
 
 void	handle_quotes(char **input, t_token **tokens)
 {
@@ -82,25 +19,22 @@ void	handle_quotes(char **input, t_token **tokens)
 	char	*quoted_content;
 
 	quote_type = **input;
-	start = ++(*input);
+	start = *input;
+	(*input)++;
 	while (**input && **input != quote_type)
 		(*input)++;
-	if (*input != start)
+	if (**input == quote_type)
 	{
-		quoted_content = strndup(start, *input - start);
+		quoted_content = strndup(start, (*input - start) + 1);
 		if (quoted_content)
 		{
 			add_token_to_list(tokens, new_token(TOKEN_WORD, quoted_content));
 			free(quoted_content);
 		}
 		else
-		{
 			ft_putstr_fd("Error: Malloc failed in handle_quotes.\n", 2);
-			/* error handling */
-		}
-	}
-	if (**input == quote_type)
 		(*input)++;
+	}
 }
 
 void	handle_special_chars(char **input, t_token **tokens)
@@ -147,10 +81,7 @@ void	handle_word(char **input, t_token **tokens)
 			free(word);
 		}
 		else
-		{
 			ft_putstr_fd("Error: Malloc failed in handle_word.\n", 2);
-			// errror handling
-		}
 	}
 }
 
@@ -169,10 +100,7 @@ void	handle_environment_variables(char **input, t_token **tokens)
 		free(var_name);
 	}
 	else
-	{
 		ft_putstr_fd("Error: Malloc failed in handle env variables.\n", 2);
-		// error handling
-	}
 }
 
 t_token	*tokenize_input(char *input)
