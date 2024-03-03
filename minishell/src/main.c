880f6722618cffd0e6f78cdd9acdba36c4029bca
Bilal Eddinaoui <beddinao@student.1337.ma>
2024-03-03 07:57:54 +0100

    general_fixes

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -40,7 +40,7 @@ void	main_shell_execution_loop(t_env *env)
 
 	while (1)
 	{
-		line = readline("\t> ");
+		line = readline("\033[1;32m→\033[1;36m\033[0m ");
 		if (!line)
 			break ;
 		if (check_line(&line))
